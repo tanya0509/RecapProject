9b11be639d57a7e93440e71ca88432faa452b719
Tanya <tanya.stamboliyska@yahoo.com>
2021-06-30 20:20:42 -0500

    User story 102 has finished

--- src/test/java/UserStory102.java
+++ src/test/java/UserStory102.java
@@ -2,5 +2,5 @@ public class UserStory102 {
     //lots of code has been written here
     //ready to push this branch to remote
     //and open pull request so I can start collaborating
-    //so my team member can comment, and review and aprove
+    //so my team member can comment, and review and approve
 }
